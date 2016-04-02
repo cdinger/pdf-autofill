b91d276c383ee2b2a7309e67c1f38d1281c87332
Chris Dinger <ding0057@umn.edu>
2016-04-02 00:05:45 -0500

    Remove commented markdown field code

--- src/clojure/pdf_autofill/fields.clj
+++ src/clojure/pdf_autofill/fields.clj
@@ -1,35 +1,4 @@
 (ns pdf-autofill.fields)
-  ;(:require [clojure.java.io :as io]
-            ;[endophile.core :as md]
-            ;[pdf-autofill.config :as config]))
-
-;(def path config/markdown-path)
-
-;(defn markdown-files []
-  ;(let [files (file-seq (io/file path))]
-    ;(rest files)))
-
-;(defn markdown-file [fieldname]
-  ;(-> (str path "/" fieldname ".md")
-      ;io/resource
-      ;io/file))
-
-;(defn markdown-content [fieldname]
-  ;(slurp (markdown-file fieldname)))
-
-;(defn markdown-map [fieldname]
-  ;(let [content (markdown-content fieldname)]
-    ;(md/to-clj (md/mp content))))
-
-;(defn sql [fieldname]
-  ;(let [m (markdown-map fieldname)]
-    ;; ick.
-    ;(-> (filter #(= :pre (get % :tag)) m)
-        ;first
-        ;(get :content)
-        ;first
-        ;(get :content)
-        ;first)))
 
 (def fields {:first_name {:description "Returns the current user's first name."
                           :sql         "select firstname from users where username = :principal_id"}
