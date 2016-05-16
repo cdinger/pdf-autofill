96b3c9c937ff484099f7d494df2f0f3c06cffd65
Chris Dinger <ding0057@umn.edu>
2016-05-15 23:21:44 -0500

    Pass current principal into fill handler

--- src/clojure/pdf_autofill/handler.clj
+++ src/clojure/pdf_autofill/handler.clj
@@ -14,11 +14,11 @@
                  [:td.fieldname] (html/content (str autofill/prefix (name (first f))))
                  [:td.description] (html/content (:description (last f)))))
 
-(defn fill-pdf [url]
+(defn fill-pdf [url principal]
   (let [doc (pdf/document url)]
     (piped-input-stream
       (fn [output-stream]
-        (pdf/save-pdf-to-stream (autofill/filled-pdf doc "test") output-stream)))))
+        (pdf/save-pdf-to-stream (autofill/filled-pdf doc principal) output-stream)))))
 
 (defn url->filename [url]
   (re-find #"(?<=\/)[^\/]*$" url))
@@ -31,7 +31,7 @@
 
 (defroutes app-routes
   (GET "/" [] (apply str (index fields)))
-  (GET "/fill" {{url :url} :params} (pdf-response (fill-pdf url) (url->filename url)))
+  (GET "/fill" {headers :headers {url :url} :params} (pdf-response (fill-pdf url (get headers "remote_user")) (url->filename url)))
   (GET "/sample.pdf" [] (pdf-response (slurp (io/resource "hello_forms.pdf")) "test.pdf"))
   (route/resources "/")
   (route/not-found (slurp (io/resource "public/404.html"))))
